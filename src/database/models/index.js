19baf9e37c9d5b650f9568d6061ed15d039bf2a0
sheyko <sheyko.wk@gmail.com>
2022-06-30 21:42:09 -0500

    test

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -10,7 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize('postgres://oqvgwckkqiuvlt:42c90b477bb791e626f35712ae28c563319854d5a3cf31a63afd0643811f8490@ec2-34-239-241-121.compute-1.amazonaws.com:5432/d1mb8deu1vqp6t', config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
