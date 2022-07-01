5dfc9c855fe5460f32615c72d209bb0eac2ed31b
sheyko <sheyko.wk@gmail.com>
2022-06-30 21:40:08 -0500

    test

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     "dialect": config.db.test.dialect
   },
   "production": {
-    "use_env_variable":`${config.db.production.url}`,
+    "use_env_variable":`postgres://oqvgwckkqiuvlt:42c90b477bb791e626f35712ae28c563319854d5a3cf31a63afd0643811f8490@ec2-34-239-241-121.compute-1.amazonaws.com:5432/d1mb8deu1vqp6t`,
     dialectOptions: {
         ssl: {
             require: true,
